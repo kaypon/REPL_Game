85e9c0101d4b88ad24a8ddc2d86cf185f393ae5d
Kevin van der Poll <vdpkevin@gmail.com>
2018-08-27 12:54:21 -0400

    Modify .sh script to check for unfinished git business

--- autoPullAndTest.sh
+++ autoPullAndTest.sh
@@ -2,10 +2,18 @@
 
 
 while [ 1 ]; do
+	echo Fetching from Master...
 	git fetch --all
-	sleep 10
+	sleep 3
+	echo Checking Status...
+	git status
+	sleep 3
 	status=$(git status)
-	if [[ $status = *"up-to-date"* ]]; then
+	
+	if [[ $status = *"Changes to be committed"* ]] | [[ $status = *"Changes not staged for commit"* ]] | [[ $status = *"Untracked files"* ]]; then
+		echo YOU HAVE CHANGES THAT ARE INTERFERING WITH THE HEALTH CHECKER. 
+		echo PLEASE ENSURE YOU HAVE NO UNSTAGED FILES OR PENDING COMMITS ON THIS BRANCH.
+	elif [[ $status = *"up-to-date"* ]]; then
 		echo repo is up-to-date
 	else
 		echo ----------------
