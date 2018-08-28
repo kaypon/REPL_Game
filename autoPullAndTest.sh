5e71ec3c6247b4ef86bf97e2c58f42695cc18869
Kevin van der Poll <vdpkevin@gmail.com>
2018-08-28 11:55:21 -0400

    remove unnecessary lines in bash file

--- autoPullAndTest.sh
+++ autoPullAndTest.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-
 while [ 1 ]; do
-	git fetch --all
+	git fetch origin
 	sleep 10
 	status=$(git status)
 	
-	if [[ $status = *"Changes to be committed"* ]] | [[ $status = *"Changes not staged for commit"* ]] | [[ $status = *"Untracked files"* ]]; then
-		echo YOU HAVE CHANGES THAT ARE INTERFERING WITH THE HEALTH CHECKER. 
-		echo PLEASE ENSURE YOU HAVE NO UNSTAGED FILES OR PENDING COMMITS ON THIS BRANCH.
-	elif [[ $status = *"up-to-date"* ]]; then
+	if [[ $status = *"up-to-date"* ]]; then
 		echo repo is up-to-date
 	else
 		echo ----------------
@@ -20,14 +16,6 @@ while [ 1 ]; do
 		sleep 5
 		git pull origin master
 		sleep 5
-		mkdir testfolder
-		cd testfolder
-		touch newfile1.txt
-		touch newfile2.txt
-		git status
-		git remote -v
-		git log
-		echo done
 	fi
 	sleep 10
 done
